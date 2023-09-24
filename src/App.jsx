cf6368a53b0305ddc99f46a7b948ed55ab171fac
Athishsreeram, Nharayanan <athishsreeram.nharayanan@bmo.com>
2023-09-24 14:01:15 -0400

    Added Diaryblog MFE

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+import Counter from "./components/Counter";
+
+
+
+const App = () => (
+  <div className="container">
+    <Counter />
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ src/components/Counter.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const onIncrement = () => setCount(count + 1);
+  const onDecrement = () => setCount(count - 1);
+
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <p>Current count: <strong>{count}</strong></p>
+      <button onClick={onIncrement}>+</button>
+      <button onClick={onDecrement}>-</button>
+    </div>
+  );
+}
+
+export default Counter;
