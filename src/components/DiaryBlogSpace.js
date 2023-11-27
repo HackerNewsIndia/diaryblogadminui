cc80e24fc6d744ff730ecdea3d4f959d822d0e62
Dinesh <dinesh.melapundi@gmail.com>
2023-11-27 15:42:43 +0530

    checking typeit space

--- src/components/DiaryBlogSpace.js
+++ src/components/DiaryBlogSpace.js
@@ -212,6 +212,66 @@ function DiaryBlogSpace({ isLoggedIn, setIsLoggedIn, selectedKey }) {
           )}
         </div>
       )}
+      {selectedKey === "typeitAdmin" && (
+        <div className="content-body">
+          {selectedCompany ? (
+            <div>
+              <h1 className="company_heading">{selectedCompany.company}</h1>
+              <button
+                className="back_button"
+                onClick={() => setSelectedCompany(null)}
+              >
+                <i className="fas fa-arrow-left" aria-hidden="true"></i>
+              </button>
+              <CompanyPosts selectedCompany={selectedCompany} />
+            </div>
+          ) : (
+            <React.Fragment>
+              <div>
+                {error && <p>Error: {error}</p>}
+                <div className="content heading_row">
+                  <h1 className="dairyBlogAdmin_h1">
+                    Web Components That Just Works.
+                  </h1>
+                </div>
+                <br />
+                <div className="row">
+                  <BlogCreator onNewBlog={handleNewBlog} />
+                </div>
+                <div className="blog-content">
+                  <h3 className="blog-h3">My Blog Space</h3>
+                  <div className="blog-card-container">
+                    {companyData &&
+                      companyData.map((blogSpace) => (
+                        <div
+                          key={blogSpace._id || blogSpace.name} // using _id as key if available, otherwise using name
+                          className="blog-card"
+                          onClick={() => handleCards(blogSpace)}
+                        >
+                          <h4 className="blog-title">{blogSpace.name}</h4>
+                          <p>
+                            <strong>Category:</strong> {blogSpace.category}
+                          </p>
+                          <p>
+                            <strong>Owner:</strong> {blogSpace.owner}
+                          </p>
+                          <p>
+                            <strong>Created Date:</strong>
+                            {blogSpace.createDate
+                              ? new Date(
+                                  blogSpace.createDate
+                                ).toLocaleDateString()
+                              : "Loading..."}
+                          </p>
+                        </div>
+                      ))}
+                  </div>
+                </div>
+              </div>
+            </React.Fragment>
+          )}
+        </div>
+      )}
     </div>
   );
 }
