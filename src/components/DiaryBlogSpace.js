c67db97046ecf5e8cc3b0d98f7ef6d23d9036ef9
VINOTH <103953710+VinothManickam@users.noreply.github.com>
2023-12-09 09:25:06 +0530

    Update DiaryBlogSpace.js

--- src/components/DiaryBlogSpace.js
+++ src/components/DiaryBlogSpace.js
@@ -258,8 +258,7 @@ function DiaryBlogSpace({ isLoggedIn, setIsLoggedIn, selectedKey }) {
                       TypeitSpaceData.typeit_spaces.map((typeitSpace) => (
                         <div
                           key={typeitSpace._id}
-                          className="blog-card" onClick={() => handleTypeitSpace(typeitSpace)}
-                          >
+                          className="blog-card">
                           <h4 className="blog-title">{typeitSpace.name}</h4>
                         </div>
                       ))}
