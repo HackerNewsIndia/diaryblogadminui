b717a975d63d7370204777a5e81580e1756b5840
VINOTH <103953710+VinothManickam@users.noreply.github.com>
2024-02-19 00:16:33 -0800

    Update DiaryBlogSpace.js

--- src/components/DiaryBlogSpace.js
+++ src/components/DiaryBlogSpace.js
@@ -14,6 +14,7 @@ import CompanyPosts from "./CompanyPosts";
 import DashboardContent from "./DashboardContents";
 import TypeitSpaceCreator from "./TypeitSpaceCreator";
 import TypeitSpacePosts from "./TypeitSpacePosts";
+import User from "./User";  
 import jwt_decode from "jwt-decode";
 
 function DiaryBlogSpace({ isLoggedIn, setIsLoggedIn, selectedKey }) {
@@ -160,6 +161,12 @@ function DiaryBlogSpace({ isLoggedIn, setIsLoggedIn, selectedKey }) {
         <div className="content-body">
           <h1 className="dashboard_heading">Welcome to Dashboard</h1>
           <DashboardContent />
+        </div>
+      )}
+   {selectedKey === "user" && (
+        <div className="content-body">
+          <h1 className="dashboard_heading">Welcome to Dashboard</h1>
+          <User />  {/* Render the User component */}
         </div>
       )}
       {selectedKey === "diaryBlogAdmin" && (
