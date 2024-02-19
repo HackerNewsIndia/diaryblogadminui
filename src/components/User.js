d50da111fcb291fcb3322892aae31c811f7921ee
VINOTH <103953710+VinothManickam@users.noreply.github.com>
2024-02-19 00:18:23 -0800

    Update User.js

--- src/components/User.js
+++ src/components/User.js
@@ -1,325 +1,239 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import "./ViewPosts.css";
-import ReactMarkdown from "react-markdown";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEye } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from "react";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
 
-const ViewPosts = () => {
-  const [posts, setPosts] = useState([]);
-  const [postSearch, setPostSearch] = useState("");
-  const { blogspace_name, blogspace_id } = useParams();
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [currentFollowCompany, setCurrentFollowCompany] = useState(null);
-  const [emailForFollow, setEmailForFollow] = useState("");
-  const [followedCompanies, setFollowedCompanies] = useState([]); // If you need this
-  const [followersCount, setFollowersCount] = useState(0);
-  const [viewMode, setViewMode] = useState("card");
-  const [sortedPosts, setSortedPosts] = useState([]);
 
-  const navigate = useNavigate();
+const ImageUploader = ({ onImageUpload }) => {
+  const uploadedImage = React.useRef(null);
+  const imageUploader = React.useRef(null);
 
-  useEffect(() => {
-    if (blogspace_id && blogspace_id !== "undefined") {
-      fetch(
-        `https://diaryblogapi2.onrender.com/api/blogspace/${blogspace_id}/posts`
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          setPosts(data);
-        })
-        .catch((error) => console.error("Error fetching posts:", error));
+  const handleImageUpload = (e) => {
+    const [file] = e.target.files;
+    if (file) {
+      const reader = new FileReader();
+      const { current } = uploadedImage;
+      current.file = file;
 
-      // Fetch follower count for the current blogSpace
-      fetch(
-        `https://diaryblogapi2.onrender.com/api/blogSpace/${blogspace_id}/followers`
-      )
-        .then((response) => response.json())
-        .then((followersData) => {
-          setFollowersCount(followersData.userEmails?.length || 0);
-        })
-        .catch((error) => {
-          console.warn(
-            `Error fetching followers for blogSpace ID: ${blogspace_id}`,
-            error
-          );
-          setFollowersCount(0);
-        });
-    } else {
-      console.error("blogspace_id is undefined!");
+      reader.onload = (e) => {
+        current.src = e.target.result;
+
+        // Get base64 representation of the image
+        const base64Image = e.target.result.split(",")[1];
+
+        // Pass base64 image data to the parent component
+        onImageUpload(base64Image);
+      };
+
+      reader.readAsDataURL(file);
     }
-  }, [blogspace_id]);
-
-  const handleChange = (e) => {
-    setPostSearch(e.target.value);
-  };
-
-  const handleBackClick = () => {
-    navigate("/diaryblogSpace");
-  };
-
-  console.log(posts);
-
-  // const filteredPosts = posts.filter((post) => {
-  //   const title = post.title || "";
-  //   const description = post.description || "";
-
-  //   return (
-  //     title.toLowerCase().includes(postSearch.toLowerCase()) ||
-  //     description.toLowerCase().includes(postSearch.toLowerCase())
-  //   );
-  // });
-
-  useEffect(() => {
-    const sorted = [...posts].sort((a, b) => {
-      const dateA = new Date(a.createDate);
-      const dateB = new Date(b.createDate);
-      return dateB - dateA;
-    });
-    setSortedPosts(sorted);
-  }, [posts]);
-
-  console.log(sortedPosts);
-
-  const filteredPosts = sortedPosts.filter((post) => {
-    const title = post.title || "";
-    const description = post.description || "";
-
-    return (
-      title.toLowerCase().includes(postSearch.toLowerCase()) ||
-      description.toLowerCase().includes(postSearch.toLowerCase())
-    );
-  });
-
-  const handlePostClick = (blogspace_name, blogSpaceId, postId) => {
-    navigate(`/company/${blogspace_name}/${blogSpaceId}/post/${postId}`);
-
-    fetch(
-      `https://diaryblogapi2.onrender.com/api/posts/${blogspace_name}/${postId}/views`,
-      {
-        method: "PUT",
-      }
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        // Update the view count locally for the post that was clicked.
-        const updatedPosts = posts.map((postItem) => {
-          if (postItem._id === postId) {
-            postItem.views = data.views; // Assuming data.views contains the updated view count.
-          }
-          return postItem;
-        });
-        setPosts(updatedPosts); // Update the state with the new post views.
-      })
-      .catch((error) => {
-        console.error("Error incrementing views:", error);
-      });
-  };
-
-  const toggleFollow = (companyName) => {
-    setCurrentFollowCompany(companyName);
-    setIsModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-    setCurrentFollowCompany(null);
-    setEmailForFollow("");
-  };
-
-  const handleConfirmFollow = () => {
-    fetch(
-      `https://diaryblogapi2.onrender.com/api/blogSpace/${blogspace_id}/follow`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email: emailForFollow }),
-      }
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.message) {
-          // No need to update the local state here, unless you want to reflect changes immediately.
-          handleCloseModal();
-        } else {
-          console.error(data.error);
-        }
-      })
-      .catch((error) => console.error("Error following company:", error));
-  };
-
-  const handleViewModeChange = (mode) => {
-    setViewMode(mode);
   };
 
   return (
-    <div>
-      <h2 className="blog-heading">{blogspace_name}'s Blog</h2>
-
-      <div style={{ textAlign: "center", margin: "10px 0" }}>
-        {/* Follow button */}
-        <button
-          className="follow-btn-post"
-          onClick={() => toggleFollow(blogspace_name)}
-        >
-          {followedCompanies.includes(blogspace_name) ? "Unfollow" : "Follow"}
-        </button>
-
-        {/* Followers count */}
-        <span style={{ marginLeft: "10px" }}>
-          {followersCount} {followersCount === 1 ? "follower" : "followers"}
-        </span>
+    <div className="flex flex-col items-center justify-center">
+      <input
+        type="file"
+        accept="image/*,application/pdf,.pdf"
+        onChange={handleImageUpload}
+        ref={imageUploader}
+        className="hidden"
+      />
+      <div
+        className="h-60 w-60 border-2 border-dashed border-black"
+        onClick={() => imageUploader.current.click()}
+      >
+        <img
+          ref={uploadedImage}
+          className="w-full h-full object-cover"
+          alt="Uploaded"
+        />
       </div>
+      <p
+        className="mt-2 cursor-pointer text-blue-500"
+        onClick={() => imageUploader.current.click()}
+      >
+        Click to upload Image
+      </p>
+    </div>
+  );
+};
 
-      {/* Modal */}
-      {isModalOpen && (
-        <div className="follow-modal-backdrop">
-          <div className="follow-modal">
-            <h2>Follow {currentFollowCompany}</h2>
-            <input
-              type="email"
-              value={emailForFollow}
-              onChange={(e) => setEmailForFollow(e.target.value)}
-              placeholder="Enter your email"
-            />
-            <button onClick={handleConfirmFollow}>Confirm Follow</button>
-            <button onClick={handleCloseModal}>Cancel</button>
-          </div>
-        </div>
-      )}
+const User = () => {
+  // console.log('User Token:', userToken);
+  const [users, setUsers] = useState([]);
+  const [userId, setUserId] = useState("");
+  const [newUserLinkedIn, setNewUserLinkedIn] = useState("");
+  const [newUserTwitter, setNewUserTwitter] = useState("");
+  const [newUserGitHub, setNewUserGitHub] = useState("");
+  const [newUserImage, setNewUserImage] = useState(null);
 
-      <form className="form-search" onSubmit={(e) => e.preventDefault()}>
-        <div className="search-group">
+  const handleLinkedInChange = (event) => {
+    setNewUserLinkedIn(event.target.value);
+  };
+
+  const handleTwitterChange = (event) => {
+    setNewUserTwitter(event.target.value);
+  };
+
+  const handleGitHubChange = (event) => {
+    setNewUserGitHub(event.target.value);
+  };
+
+  const handleImageUpload = (base64Image) => {
+    console.log("Base64 Image Data:", base64Image);
+    setNewUserImage(base64Image);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        console.log("Token not found in local storage");
+        return;
+      }
+
+      const decodedToken = jwt_decode(token);
+      const userId = decodedToken.id;
+
+      const userData = {
+        user_id: userId,
+        linkedin: newUserLinkedIn,
+        twitter: newUserTwitter,
+        github: newUserGitHub,
+        image_base64: newUserImage,
+      };
+
+      const response = await axios.post(
+        "http://127.0.0.1:5000/api/update_user",
+        userData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            // Authorization: `Bearer ${userId}`,
+          },
+        }
+      );
+
+      const newUser = response.data;
+
+      setUsers((prevUsers) => [...prevUsers, newUser]);
+      setNewUserLinkedIn("");
+      setNewUserTwitter("");
+      setNewUserGitHub("");
+      setNewUserImage("");
+      alert("User saved successfully!");
+    } catch (error) {
+      console.error("Error adding new user:", error);
+      console.error("Error details:", error.toJSON());
+      if (error.response) {
+        console.error("Server responded with:", error.response.data);
+      } else if (error.request) {
+        console.error("No response received from the server");
+      } else {
+        console.error("Error setting up the request:", error.message);
+      }
+      alert("Error adding a new user. Please try again.");
+    }
+  };
+
+  return (
+    <div className="flex min-h-full items-center justify-center px-6 py-12 lg:px-8">
+      <div className="bg-white rounded-lg shadow-lg p-8 max-w-md w-full">
+        <form onSubmit={handleSubmit} className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            LinkedIn URL:
+          </label>
           <input
-            className="search-input"
             type="text"
-            placeholder="search posts, company"
-            value={postSearch}
-            onChange={handleChange}
+            value={newUserLinkedIn}
+            onChange={handleLinkedInChange}
+            className="w-full border border-gray-300 rounded-md py-2 px-3"
           />
-        </div>
-      </form>
-      <section className="category-section">
-        <div className="category-chips">
-          <div className="chip" data-category="all">
-            All
-          </div>
-          <div class="chip" data-category="lastposted">
-            Recent Posted
-          </div>
-          <div class="chip" data-category="lastposted">
-            Most Trending Posted
-          </div>
-          <div class="chip" data-category="geography">
-            Geography
-          </div>
-          <div class="chip" data-category="science">
-            Science
-          </div>
-          <div class="chip" data-category="history">
-            History
-          </div>
-        </div>
-      </section>
+          <label className="block text-gray-700 text-sm font-bold mb-2 mt-4">
+            Twitter URL:
+          </label>
+          <input
+            type="text"
+            value={newUserTwitter}
+            onChange={handleTwitterChange}
+            className="w-full border border-gray-300 rounded-md py-2 px-3"
+          />
+          <label className="block text-gray-700 text-sm font-bold mb-2 mt-4">
+            GitHub URL:
+          </label>
+          <input
+            type="text"
+            value={newUserGitHub}
+            onChange={handleGitHubChange}
+            className="w-full border border-gray-300 rounded-md py-2 px-3"
+          />
 
-      <div className="card-container">
-        <div className="back-button-container">
-          <button className="post-back" onClick={handleBackClick}>
-            <i className="fas fa-arrow-left" aria-hidden="true"></i>
-          </button>
-          <div className="card-list">
+          {/* Add ImageUploader component */}
+          <ImageUploader onImageUpload={handleImageUpload} />
+
+          <div className="flex justify-center mt-4">
             <button
-              className="card-view"
-              onClick={() => handleViewModeChange("card")}
+              type="submit"
+              className="mt-4 bg-blue-500 text-white py-2 px-4 rounded-md "
             >
-              <i class="material-icons">grid_on</i>
-            </button>
-            <button
-              className="list-view"
-              onClick={() => handleViewModeChange("list")}
-            >
-              <i class="material-icons">list</i>
+              Save
             </button>
           </div>
-        </div>
-        {viewMode === "card" ? (
-          <div className="card-container">
-            {filteredPosts.map((post, index) => (
-              <div
-                key={index}
-                className="col-sm-3"
-                onClick={() =>
-                  handlePostClick(blogspace_name, post.blogSpace, post._id)
-                }
-              >
-                <div className="card text-center">
-                  <img
-                    src={
-                      post.imageUrl ||
-                      "https://assets.hongkiat.com/uploads/psd-text-svg/logo-example.jpg"
-                    }
-                    className="card-img-top"
-                    alt={"image"}
-                  />
-                  <div className="post-body custom-card-body">
-                    <h2 className="post-title">
-                      <ReactMarkdown>
-                        {post.title.substring(0, 30)}
-                      </ReactMarkdown>
-                    </h2>
-                    <p className="post-text">~{post.author}</p>
-                    <p className="post-views">
-                      <FontAwesomeIcon icon={faEye} />: {post.views}
-                    </p>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <div className="list-head-container">
-            <ul className="list-container">
-              {filteredPosts.map((post, index) => (
-                <li key={index}>
-                  <div
-                    onClick={() =>
-                      handlePostClick(blogspace_name, post.blogSpace, post._id)
-                    }
-                    className="list-item"
+        </form>
+
+        <div className="mx-auto grid max-w-4xl grid-cols-1 gap-4 sm-p:md:grid-cols-2">
+          {users.map((user) => (
+            <div key={user._id}>
+              <div className="flex justify-around mt-4">
+                {user.linkedin && (
+                  <a
+                    href={user.linkedin}
+                    className="text-blue-500 hover:underline"
+                    target="_blank"
+                    rel="noopener noreferrer"
                   >
-                    <img
-                      src={
-                        post.imageUrl ||
-                        "https://assets.hongkiat.com/uploads/psd-text-svg/logo-example.jpg"
-                      }
-                      alt={"image"}
-                      className="list-image"
-                    />
-                    <div className="list-content">
-                      <h2 className="post-title-list">
-                        <ReactMarkdown className="title-text">
-                          {post.title.substring(0, 30)}
-                        </ReactMarkdown>
-                      </h2>
-                      <div className="post-text-company-list">
-                        <p className="post-text-list">
-                          {post.description.substring(0, 90)}...{" "}
-                          {/* Display only first 100 characters */}
-                        </p>
-                        <p className="post-company-list">~{post.author}</p>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
+                    LinkedIn
+                  </a>
+                )}
+                {user.github && (
+                  <a
+                    href={user.github}
+                    className="text-blue-500 hover:underline"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    GitHub
+                  </a>
+                )}
+                {user.twitter && (
+                  <a
+                    href={user.twitter}
+                    className="text-blue-500 hover:underline"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Twitter
+                  </a>
+                )}
+              </div>
+
+              {/* Display user image */}
+              {user.image && (
+                <div className="mt-4 ">
+                  <img
+                    src={`https://usermgtapi3.onrender.com/${user.image}`}
+                    alt="User"
+                    className="w-full h-auto"
+                  />
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
 };
 
-export default ViewPosts;
+export default User;
