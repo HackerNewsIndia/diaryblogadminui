e5cd0713d510e5263eb559ea9ee557b2f14d9c3e
VINOTH <103953710+VinothManickam@users.noreply.github.com>
2024-02-29 19:19:45 -0800

    Update CreateUserBlog.js

--- src/components/CreateUserBlog.js
+++ src/components/CreateUserBlog.js
@@ -68,78 +68,79 @@ function CreateUserBlog({ onClose, onNewBlog }) {
     }
   };
  return (
-    <div className="bg-white shadow-md rounded-md p-6 w-full max-w-md">
-      <div className="flex items-center justify-between mb-4">
-        <h3 className="text-xl font-bold text-blue-500 mb-2">Create blog </h3>
-        <button className="cancel-button text-red-600" onClick={onClose}>
-          ❌
-        </button>
-      </div>
-      <form className="createBlogForm" onSubmit={handleSubmit}>
-        <label className="block text-gray-700 text-sm font-bold mb-2 sm:mb-4">
-          Title:
-        </label>
-        <input
-          type="text"
-          className="blogInput border p-2 mb-2 w-full"
-          placeholder="Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        {errors.title && (
-          <p className="error text-red-500">{errors.title}</p>
-        )}
-        <label className="block text-gray-700 text-sm font-bold mb-2 sm:mb-4">
-          URL:
-        </label>
-        <input
-          type="url"
-          className="blogInput border p-2 mb-2 w-full"
-          placeholder="URL"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-        />
-        {errors.url && <p className="error text-red-500">{errors.url}</p>}
-        <label className="block text-gray-700 text-sm font-bold mb-2 sm:mb-4">
-          Image:
-        </label>
-        <input
-          type="text"
-          className="blogInput border p-2 mb-2 w-full"
-          placeholder="Image URL"
-          value={image_Url}
-          onChange={(e) => setImageUrl(e.target.value)}
-        />
-        <label className="block text-gray-700 text-sm font-bold mb-2 sm:mb-4">
-          Category:
-        </label>
-        <select
-          className="blogInput border p-2 mb-2 w-full sm:mb-4"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-        >
-          <option value="">Select a category</option>
-          <option value="Lifestyle">Lifestyle</option>
-          <option value="Travel">Technology</option>
-          <option value="Food and Recipes">Food and Recipes</option>
-          <option value="Personal Finance">Personal Finance</option>
-          <option value="Parenting and Family">Parenting and Family</option>
-        </select>
-        {errors.category && (
-          <p className="error text-red-500">{errors.category}</p>
-        )}
-        {success && (
-          <p className="success text-green-500">Blog space created successfully!</p>
-        )}
-        <button
-          type="submit"
-          className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 w-full mt-4"
-        >
-          Submit
-        </button>
-      </form>
-    </div>
-  );
-}
-
-export default CreateUserBlog;
+      <div className="form-container max-w-lg mx-auto p-4">
+          <div className="form-header flex items-center justify-between mb-4">
+            <h3 className="text-xl font-bold text-blue-500">Create blog </h3>
+            <button className="cancel-button text-red-600" onClick={onClose}>
+              ❌
+            </button>
+          </div>
+          <form className="createBlogForm" onSubmit={handleSubmit}>
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Title:
+            </label>
+            <input
+              type="text"
+              className="blogInput border p-2 mb-2 w-full"
+              placeholder="Title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            {errors.title && (
+              <p className="error text-red-500">{errors.title}</p>
+            )}
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              URL:
+            </label>
+            <input
+              type="url"
+              className="blogInput border p-2 mb-2 w-full"
+              placeholder="URL"
+              value={url}
+              onChange={(e) => setUrl(e.target.value)}
+            />
+            {errors.url && <p className="error text-red-500">{errors.url}</p>}
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Image:
+            </label>
+            <input
+              type="text"
+              className="blogInput border p-2 mb-2 w-full"
+              placeholder="Image URL"
+              value={image_Url}
+              onChange={(e) => setImageUrl(e.target.value)}
+            />
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Category:
+            </label>
+            <select
+              className="blogInput border p-2 mb-2 w-full"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <option value="">Select a category</option>
+              <option value="Lifestyle">Lifestyle</option>
+              <option value="Travel">Technology</option>
+              <option value="Food and Recipes">Food and Recipes</option>
+              <option value="Personal Finance">Personal Finance</option>
+              <option value="Parenting and Family">Parenting and Family</option>
+            </select>
+            {errors.category && (
+              <p className="error text-red-500">{errors.category}</p>
+            )}
+            {success && (
+              <p className="success text-green-500">Blog space created successfully!</p>
+            )}
+            <button
+              type="submit"
+              className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 w-full"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      );
+    }
+    
+    export default CreateUserBlog;
+    
