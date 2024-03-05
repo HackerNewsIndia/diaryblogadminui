80ec905881c99fd2361d09c111aa3187c8fc1f0e
athishsreeram <athishsreeram007@gmail.com>
2024-03-05 00:45:58 -0500

    Update CreateUserBlog.js style to be mobile compliant

--- src/components/CreateUserBlog.js
+++ src/components/CreateUserBlog.js
@@ -68,75 +68,116 @@ function CreateUserBlog({ onClose, onNewBlog }) {
     }
   };
  return (
-      <div className="form-container max-w-lg mx-auto p-4 sm:w-full">
-          <div className="form-header flex items-center justify-between mb-4">
-            <h3 className="text-xl font-bold text-blue-500 sm:text-lg">Create blog </h3>
-            <button className="cancel-button text-red-600 text-sm sm:text-base" onClick={onClose}>
-              ❌
-            </button>
+          <div className="flex min-h-full items-center justify-center px-6 py-12 lg:px-8">
+            <div className="bg-white rounded-lg shadow-lg p-8 max-w-md w-full">
+              <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                  Create blog
+                </h2>
+                <button
+                      onClick={onClose}
+                      className="cancel-button text-red-600 text-sm sm:text-base"
+                    >
+                      ❌
+                  </button>
+              </div>
+          
+              <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form className="space-y-6 createBlogForm" onSubmit={handleSubmit}>
+                  <div>
+                    <label
+                      htmlFor="title"
+                      className="block text-md font-medium leading-6 text-gray-900"
+                    >
+                      Title:
+                    </label>
+                    <input
+                      id="title"
+                      type="text"
+                      className="block w-full rounded-md border-2 py-1.5 text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-400 sm:text-sm sm:leading-6"
+                      placeholder="Title"
+                      value={title}
+                      onChange={(e) => setTitle(e.target.value)}
+                    />
+                    {errors.title && (
+                      <p className="mt-2 text-sm text-red-500">{errors.title}</p>
+                    )}
+                  </div>
+          
+                  <div>
+                    <label
+                      htmlFor="url"
+                      className="block text-md font-medium leading-6 text-gray-900"
+                    >
+                      URL:
+                    </label>
+                    <input
+                      id="url"
+                      type="url"
+                      className="block w-full rounded-md border-2 py-1.5 text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-400 sm:text-sm sm:leading-6"
+                      placeholder="URL"
+                      value={url}
+                      onChange={(e) => setUrl(e.target.value)}
+                    />
+                    {errors.url && (
+                      <p className="mt-2 text-sm text-red-500">{errors.url}</p>
+                    )}
+                  </div>
+          
+                  <div>
+                    <label
+                      htmlFor="image"
+                      className="block text-md font-medium leading-6 text-gray-900"
+                    >
+                      Image:
+                    </label>
+                    <input
+                      id="image"
+                      type="text"
+                      className="block w-full rounded-md border-2 py-1.5 text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-400 sm:text-sm sm:leading-6"
+                      placeholder="Image URL"
+                      value={image_Url}
+                      onChange={(e) => setImageUrl(e.target.value)}
+                    />
+                  </div>
+          
+                  <div>
+                    <label
+                      htmlFor="category"
+                      className="block text-md font-medium leading-6 text-gray-900"
+                    >
+                      Category:
+                    </label>
+                    <select
+                      id="category"
+                      className="block w-full rounded-md border-2 py-1.5 text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-400 sm:text-sm sm:leading-6"
+                      value={category}
+                      onChange={(e) => setCategory(e.target.value)}
+                    >
+                      <option value="">Select a category</option>
+                      <option value="Lifestyle">Lifestyle</option>
+                      <option value="Travel">Technology</option>
+                      <option value="Food and Recipes">Food and Recipes</option>
+                      <option value="Personal Finance">Personal Finance</option>
+                      <option value="Parenting and Family">Parenting and Family</option>
+                    </select>
+                    {errors.category && (
+                      <p className="mt-2 text-sm text-red-500">{errors.category}</p>
+                    )}
+                  </div>
+          
+                  <div>
+                    <button
+                      type="submit"
+                      className="flex w-full justify-center rounded-md bg-blue-500 px-3 py-1.5 text-md font-semibold leading-6 text-white shadow-sm hover:bg-blue-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
           </div>
-          <form className="createBlogForm" onSubmit={handleSubmit}>
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Title:
-            </label>
-            <input
-              type="text"
-              className="blogInput border p-2 mb-2 w-full sm:mb-1"
-              placeholder="Title"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-            {errors.title && (
-              <p className="error text-red-500">{errors.title}</p>
-            )}
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              URL:
-            </label>
-            <input
-              type="url"
-              className="blogInput border p-2 mb-2 w-full sm:mb-1"
-              placeholder="URL"
-              value={url}
-              onChange={(e) => setUrl(e.target.value)}
-            />
-            {errors.url && <p className="error text-red-500">{errors.url}</p>}
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Image:
-            </label>
-            <input
-              type="text"
-              className="blogInput border p-2 mb-2 w-full sm:mb-1"
-              placeholder="Image URL"
-              value={image_Url}
-              onChange={(e) => setImageUrl(e.target.value)}
-            />
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Category:
-            </label>
-            <select
-              className="blogInput border p-2 mb-2 w-full sm:mb-1"
-              value={category}
-              onChange={(e) => setCategory(e.target.value)}
-            >
-              <option value="">Select a category</option>
-              <option value="Lifestyle">Lifestyle</option>
-              <option value="Travel">Technology</option>
-              <option value="Food and Recipes">Food and Recipes</option>
-              <option value="Personal Finance">Personal Finance</option>
-              <option value="Parenting and Family">Parenting and Family</option>
-            </select>
-            {errors.category && (
-              <p className="error text-red-500">{errors.category}</p>
-            )}
-           
-            <button
-              type="submit"
-              className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 w-full sm:w-auto"
-            >
-              Submit
-            </button>
-          </form>
-        </div>
       );
     }
     
