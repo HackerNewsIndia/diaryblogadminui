1c569d881dfa7e435fe5106a2c82e2950c635b6b
VINOTH <103953710+VinothManickam@users.noreply.github.com>
2024-03-13 20:41:19 +0530

    Update PostsTable.js

--- src/components/PostsTable.js
+++ src/components/PostsTable.js
@@ -289,7 +289,7 @@ useEffect(() => {
           <div className="relative">
 
           <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                  <svg
+      {/* <svg
                     className="w-4 h-4 text-gray-500"
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
@@ -303,7 +303,7 @@ useEffect(() => {
                       strokeWidth="2"
                       d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
                     />
-                  </svg>
+                  </svg> */}
                 </div>
           <input
                   type="search"
