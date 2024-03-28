c3f63670fafc9317bd3474e10a9861956e5b5f8b
VINOTH <103953710+VinothManickam@users.noreply.github.com>
2024-03-28 11:19:11 +0530

    Update DashboardContents.js

--- src/components/DashboardContents.js
+++ src/components/DashboardContents.js
@@ -444,9 +444,10 @@ const DashboardContent = () => {
                           <div>
                             <div class="flex flex-wrap justify-between">
                               <div class="flex space-x-2 text-sm dark:text-gray-400">
-                                <button
-                                  aria-label="Share this post"
-                                  type="button"
+                                <a
+                                  href={`https://diaryblog.connectingpeopletech.com/${post.blogSpace}/${post._id}/post`}
+                                  target="_blank"
+                                  rel="noopener noreferrer"
                                   class="flex items-center p-1 space-x-2"
                                 >
                                   <svg
@@ -461,8 +462,8 @@ const DashboardContent = () => {
                                       <path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"></path>
                                     </svg>
                                   </svg>
-                                  <span>view</span>
-                                </button>
+                                  <span>View</span>
+                                </a>
                                 <button
                                   type="button"
                                   class="flex items-center p-1 space-x-1.5"
@@ -566,9 +567,10 @@ const DashboardContent = () => {
                           <div>
                             <div class="flex flex-wrap justify-between">
                               <div class="flex space-x-2 text-sm dark:text-gray-400">
-                                <button
-                                  aria-label="Share this post"
-                                  type="button"
+                                 <a
+                                  href={`https://diaryblog.connectingpeopletech.com/${post.blogSpace}/${post._id}/post`}
+                                  target="_blank"
+                                  rel="noopener noreferrer"
                                   class="flex items-center p-1 space-x-2"
                                 >
                                   <svg
@@ -583,8 +585,8 @@ const DashboardContent = () => {
                                       <path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"></path>
                                     </svg>
                                   </svg>
-                                  <span>view</span>
-                                </button>
+                                  <span>View</span>
+                                </a>
                                 <button
                                   type="button"
                                   class="flex items-center p-1 space-x-1.5"
@@ -686,9 +688,10 @@ const DashboardContent = () => {
                           <div>
                             <div class="flex flex-wrap justify-between">
                               <div class="flex space-x-2 text-sm dark:text-gray-400">
-                                <button
-                                  aria-label="Share this post"
-                                  type="button"
+                                <a
+                                  href={`https://diaryblog.connectingpeopletech.com/${post.blogSpace}/${post._id}/post`}
+                                  target="_blank"
+                                  rel="noopener noreferrer"
                                   class="flex items-center p-1 space-x-2"
                                 >
                                   <svg
@@ -703,8 +706,8 @@ const DashboardContent = () => {
                                       <path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"></path>
                                     </svg>
                                   </svg>
-                                  <span>view</span>
-                                </button>
+                                  <span>View</span>
+                                </a>
                                 <button
                                   type="button"
                                   class="flex items-center p-1 space-x-1.5"
@@ -814,9 +817,10 @@ const DashboardContent = () => {
                           <div>
                             <div class="flex flex-wrap justify-between">
                               <div class="flex space-x-2 text-sm dark:text-gray-400">
-                                <button
-                                  aria-label="Share this post"
-                                  type="button"
+                                <a
+                                  href={`https://diaryblog.connectingpeopletech.com/${post.blogSpace}/${post._id}/post`}
+                                  target="_blank"
+                                  rel="noopener noreferrer"
                                   class="flex items-center p-1 space-x-2"
                                 >
                                   <svg
@@ -831,8 +835,8 @@ const DashboardContent = () => {
                                       <path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"></path>
                                     </svg>
                                   </svg>
-                                  <span>view</span>
-                                </button>
+                                  <span>View</span>
+                                </a>
                                 <button
                                   type="button"
                                   class="flex items-center p-1 space-x-1.5"
@@ -936,9 +940,10 @@ const DashboardContent = () => {
                           <div>
                             <div class="flex flex-wrap justify-between">
                               <div class="flex space-x-2 text-sm dark:text-gray-400">
-                                <button
-                                  aria-label="Share this post"
-                                  type="button"
+                                <a
+                                  href={`https://diaryblog.connectingpeopletech.com/${post.blogSpace}/${post._id}/post`}
+                                  target="_blank"
+                                  rel="noopener noreferrer"
                                   class="flex items-center p-1 space-x-2"
                                 >
                                   <svg
@@ -952,9 +957,9 @@ const DashboardContent = () => {
                                     >
                                       <path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"></path>
                                     </svg>
-                                    <span>view</span>
                                   </svg>
-                                </button>
+                                  <span>View</span>
+                                </a>
                                 <button
                                   type="button"
                                   class="flex items-center p-1 space-x-1.5"
@@ -1058,9 +1063,10 @@ const DashboardContent = () => {
                           <div>
                             <div class="flex flex-wrap justify-between">
                               <div class="flex space-x-2 text-sm dark:text-gray-400">
-                                <button
-                                  aria-label="Share this post"
-                                  type="button"
+                                <a
+                                  href={`https://diaryblog.connectingpeopletech.com/${post.blogSpace}/${post._id}/post`}
+                                  target="_blank"
+                                  rel="noopener noreferrer"
                                   class="flex items-center p-1 space-x-2"
                                 >
                                   <svg
@@ -1074,9 +1080,9 @@ const DashboardContent = () => {
                                     >
                                       <path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"></path>
                                     </svg>
-                                    <span>view</span>
                                   </svg>
-                                </button>
+                                  <span>View</span>
+                                </a>
                                 <button
                                   type="button"
                                   class="flex items-center p-1 space-x-1.5"
@@ -1178,9 +1184,10 @@ const DashboardContent = () => {
                           <div>
                             <div class="flex flex-wrap justify-between">
                               <div class="flex space-x-2 text-sm dark:text-gray-400">
-                                <button
-                                  aria-label="Share this post"
-                                  type="button"
+                                <a
+                                  href={`https://diaryblog.connectingpeopletech.com/${post.blogSpace}/${post._id}/post`}
+                                  target="_blank"
+                                  rel="noopener noreferrer"
                                   class="flex items-center p-1 space-x-2"
                                 >
                                   <svg
@@ -1194,9 +1201,9 @@ const DashboardContent = () => {
                                     >
                                       <path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"></path>
                                     </svg>
-                                    <span>view</span>
                                   </svg>
-                                </button>
+                                  <span>View</span>
+                                </a>
                                 <button
                                   type="button"
                                   class="flex items-center p-1 space-x-1.5"
