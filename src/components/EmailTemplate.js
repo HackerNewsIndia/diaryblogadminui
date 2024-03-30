542de7d3ee702f1bbd059a770a6ac6c89591777c
Dinesh <dinesh.melapundi@gmail.com>
2024-03-30 18:48:00 +0530

    removed image base64

--- src/components/EmailTemplate.js
+++ src/components/EmailTemplate.js
@@ -15,6 +15,9 @@ const EmailTemplate = ({ draftPostData }) => {
   const [blogSpaceImageUrl, setBlogSpaceImageUrl] = useState("");
   const [blogSpaceName, setBlogSpaceName] = useState("");
   const [markdownDescription, setMarkdownDescription] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [emailSent, setEmailSent] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     const markdownToJSX = (postDescription) => {
@@ -152,16 +155,10 @@ const EmailTemplate = ({ draftPostData }) => {
 
   const handleSendEmail = async () => {
     try {
-      // Basic input validation
-      // if (!recipient.trim()) {
-      //   alert("Recipient email address is required.");
-      //   return;
-      // }
+      setLoading(true);
 
-      // Assuming you have an API endpoint '/api/send_email_new_post' for sending emails
       const response = await fetch(
         "https://diaryblogapi2.onrender.com/api/send_email_new_post",
-        // "http://127.0.0.1:5001/api/send_email_new_post",
         {
           method: "POST",
           headers: {
@@ -171,12 +168,11 @@ const EmailTemplate = ({ draftPostData }) => {
             post_title: postTitle,
             post_description: markdownDescription,
             post_imageUrl: postImageUrl,
-            // recipients: [recipient],
             blogId: blogId,
             postId: postId,
-            imageBase64: imageBase64,
+            // imageBase64: imageBase64,
             username: username,
-            blogSpaceImageUrl: blogSpaceImageUrl,
+            // blogSpaceImageUrl: blogSpaceImageUrl,
             blogSpaceName: blogSpaceName,
             userId: userId,
           }),
@@ -184,13 +180,15 @@ const EmailTemplate = ({ draftPostData }) => {
       );
 
       if (response.ok) {
-        alert("Email sent successfully!");
+        setEmailSent(true);
       } else {
         throw new Error("Failed to send email.");
       }
     } catch (error) {
+      setErrorMessage("Failed to send email. Please try again later.");
       console.error("Error sending email:", error.message);
-      alert("Failed to send email. Please try again later.");
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -389,18 +387,44 @@ const EmailTemplate = ({ draftPostData }) => {
         </table>
         <button
           onClick={handleSendEmail}
-          style={{
-            marginTop: "20px",
-            padding: "10px 20px",
-            borderRadius: "4px",
-            backgroundColor: "#007bff",
-            color: "#fff",
-            border: "none",
-            cursor: "pointer",
-          }}
+          className="flex w-56 justify-center rounded-md bg-indigo-600 mt-4 px-3 py-1.5 text-md font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          disabled={loading || emailSent}
         >
-          Send Email
+          {loading ? (
+            <span className="flex items-center">
+              <span className="mr-2">Loading</span>
+              <svg
+                className="animate-spin h-5 w-5 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+            </span>
+          ) : emailSent ? (
+            "Email Sent"
+          ) : (
+            "Send Email"
+          )}
         </button>
+        {errorMessage && (
+          <p className="mt-2" style={{ color: "red" }}>
+            {errorMessage}
+          </p>
+        )}
       </body>
     </html>
   );
