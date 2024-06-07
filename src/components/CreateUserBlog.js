7de29d791f132cf3aafc7b2ed1d371cc08ec0648
VINOTH <103953710+VinothManickam@users.noreply.github.com>
2024-06-07 07:49:19 +0530

    Update CreateUserBlog.js

--- src/components/CreateUserBlog.js
+++ src/components/CreateUserBlog.js
@@ -47,7 +47,7 @@ function CreateUserBlog({ onClose, onNewBlog }) {
             Authorization: `Bearer ${token}`,
           },
           body: JSON.stringify({
-            name: title,
+            name: title.trim(), // Trim the title before sending
             description: description,
             image_url: image_Url,
             category: category,
